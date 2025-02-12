2adc8654a475f726cb2525ace8484acde381758d
Akbar <you@example.com>
2025-02-11 20:09:59 -0500

    day9

---
+++ src/test/java/io/loop/test/day9/T4_drag_and_drop_POM.java
@@ -0,0 +1,4 @@
+package io.loop.test.day9;
+
+public class T4_drag_and_drop_POM {
+}
--- src/test/java/io/loop/test/hometask/day7/task2.java
+++ src/test/java/io/loop/test/hometask/day7/task2.java
@@ -1,13 +1,12 @@
 package io.loop.test.hometask.day7;
 
 import io.loop.test.base.TestBase;
-import io.loop.test.utilities.ConfigurationReader;
-import io.loop.test.utilities.DocuportConstants;
-import io.loop.test.utilities.DocuportUtils;
-import io.loop.test.utilities.Driver;
+import io.loop.test.utilities.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class task2 {
     /*
@@ -19,13 +18,39 @@ public class task2 {
     6. validate that role for alex.de.souza@gmail.com is client
     7. validate that advisor for alex.de.souza@gmail.com is  Batch1 Group1
      */
-
+    String actual, expected;
     @Test
     public void testAdvisor() throws InterruptedException {
         // login as an advisor
         Driver.getDriver().get(ConfigurationReader.getProperties("env"));
-        Thread.sleep(5000);
+
         DocuportUtils.login(Driver.getDriver(), "advisor");
+        //go to users
+        WebElement users = Driver.getDriver().findElement(By.xpath("//span[.='Users']"));
+        users.click();
+        //3. validate that full name for alex.de.souza@gmail.com is Alex De Souza - we did in class
+        expected = "alexdesouze";
+        actual = DocuportWebTableUtils.returnAnyField(Driver.getDriver(), "alex.de.souza@gmail.com", "username");
+        assertEquals(actual, expected, "Actual does not match expected");
+
+        //validate that phone number for alex.de.souza@gmail.com is +994512060042
+         expected = "+994512060042";
+         actual = DocuportWebTableUtils.returnAnyField(Driver.getDriver(), "alex.de.souza@gmail.com", "phone number");
+        assertEquals(actual, expected, "Actual does not match expected");
+
+        //validate that role for alex.de.souza@gmail.com is client
+        expected = "Client";
+        actual = DocuportWebTableUtils.returnAnyField(Driver.getDriver(), "alex.de.souza@gmail.com", "role");
+        assertEquals(actual, expected, "Actual does not match expected");
+
+        //validate that advisor for alex.de.souza@gmail.com is  Batch1 Group1
+
+        expected = "Batch1 Group1";
+        actual = DocuportWebTableUtils.returnAnyField(Driver.getDriver(), "alex.de.souza@gmail.com", "advisor");
+        assertEquals(actual, expected, "Actual does not match expected");
+
+
+
 
 
 
